7f25c9961b68f3429004a123ac78f09302bfaeca
Emanuel <emanuelgoncalves287@gmail.com>
2024-08-10 13:31:22 -0300

    p

---
+++ aula01/aula01.js
@@ -0,0 +1,69 @@
+function qualmeunome(){
+    const name = 'Emanuel'
+    console.log("Emanuel Gonçalves")
+}
+
+qualmeunome
+
+function qualquernome(name){
+    console.log(name)
+}
+
+qualquernome()
+qualquernome()
+qualquernome()
+qualquernome()
+
+function somanumeros(numero1, numero2){
+    const soma = numero1 + numero2
+
+    console.log(soma)
+}
+
+somanumeros()
+
+
+function soma(numero1, numero2){
+    const resultado = numero1 + numero2
+
+    return resultado
+}
+
+const resultadodasoma = soma()
+
+console.log(resultadodasoma)
+
+function estaendividado(receita, gastos){
+    if(receita >gastos){
+        return "Está no Azul"
+    } else {
+        return "Está no vermelho"
+    }
+}
+
+const maria = estaendividado(10, 5)
+
+const eduardo = estaendividado(5, 10)
+
+console.log()
+console.log()
+
+function abacates(){}
+const digameunome2 = () => {
+    console.log("Emanuel")
+}
+
+digameunome2()
+
+
+
+
+
+
+
+
+
+
+
+
+
